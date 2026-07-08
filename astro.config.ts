7421ed74bd742039bee875d8fcaa5e1593b980c9
Alex Lin <38541529+alxn3@users.noreply.github.com>
2026-07-08 00:21:19 -0400

    Styling variables and base layout

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import alphinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [alphinejs()],
 });
